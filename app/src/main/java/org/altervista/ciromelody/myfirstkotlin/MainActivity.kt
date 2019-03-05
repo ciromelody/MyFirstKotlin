a519dc392bfccfa99067fc517e8ec72569b930b2
Ciromelody <ciromelody@gmail.com>
2019-03-05 21:47:51 +0100

    due definizione di tipi var e val

--- app/src/main/java/org/altervista/ciromelody/myfirstkotlin/MainActivity.kt
+++ app/src/main/java/org/altervista/ciromelody/myfirstkotlin/MainActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 var string:Array<String> = arrayOf("ciao","ciao2")
+val valoreimmutabile:String="valorechenonmuta"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

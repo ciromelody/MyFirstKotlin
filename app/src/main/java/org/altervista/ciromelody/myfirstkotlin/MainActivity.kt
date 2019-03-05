176a204c3f92b77cb5148b78779a86e027736a6e
Ciromelody <ciromelody@gmail.com>
2019-03-05 19:21:43 +0100

    initial commit

---
+++ app/src/main/java/org/altervista/ciromelody/myfirstkotlin/MainActivity.kt
@@ -0,0 +1,17 @@
+package org.altervista.ciromelody.myfirstkotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+var string:Array<String> = arrayOf("ciao","ciao2")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        main(string)
+    }
+    fun main(args:Array<String> ){
+       println(args[1])
+
+    }
+}
